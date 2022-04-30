82dc57da951bdec37e81fbb1f7b8207f2f13130c
Jan Ullmann <102742052+ullmannJan@users.noreply.github.com>
2022-04-30 18:31:00 +0200

    implemented function that only returns the derivative

--- bSplines.hpp
+++ bSplines.hpp
@@ -61,7 +61,6 @@ std::cout << ndxBsplines(splines, knots, index, orderDeriv) << std::endl;
 
  * @endcode
  * 
- *         
  * @param splines An array of length k (order of B-splines to calulate derivative of)
  *                that contains the correct non-zero B-splines of order k-n where n is 
  *                the degree of the derivative.
@@ -72,7 +71,7 @@ std::cout << ndxBsplines(splines, knots, index, orderDeriv) << std::endl;
  * @return ArrayXd of length k that contains the derivative values of the non-zero derivatives
  *         of splines B_k,i(x)
  */
-ArrayXd ndxBsplines(ArrayXd &splines, ArrayXd &knotsInput, int index, uint nthDeriv=1)
+ArrayXd ndxBsplinesHelper(ArrayXd &splines, ArrayXd &knotsInput, int index, uint nDeriv=1)
 {
     int kOrd = splines.size();
 
@@ -84,7 +83,7 @@ ArrayXd ndxBsplines(ArrayXd &splines, ArrayXd &knotsInput, int index, uint nthDe
     ArrayXd deriv = splines;
 
     // this loop transforms the splines array into the correct derivative
-    for (int n = nthDeriv; n > 0; n--)
+    for (int n = nDeriv; n > 0; n--)
     {
         // do the actual calculation
 
@@ -192,11 +191,102 @@ std::tuple<MatrixXd, int> bSplinesWithDeriv(double x, ArrayXd &knotsInput, int k
 
         if (k > kOrd-nDeriv-2)
         {
-            output.col(kOrd-k-1) = ndxBsplines(splines, knots, i, kOrd-k-1);
+            output.col(kOrd-k-1) = ndxBsplinesHelper(splines, knots, i, kOrd-k-1);
         }
     }
 
     output.col(0) = splines;
+    return {output, i};
+
+}
+
+
+/**
+ * @brief Function that returns n-th derivative of non-zero splines at position x.
+ *        It also returns an index of the spline furthest to the right that is non-zero.
+ *        Ghost points on both sides of the knots are automatically generated.
+ *        The splines are ordered in descending order e.g. [B_i, B_i-1, B_i-2, ... , B_i-kOrd+1]
+ * 
+ * @code
+#include <Eigen/Dense>
+#include <iostream>
+#include <tuple>
+using namespace Eigen;
+
+...
+
+int kOrd = 4;
+ArrayXd knots = ArrayXd::LinSpaced(11,0,1);
+
+double x = 0.52;
+int orderDeriv = 2;
+auto [arr, b] = ndxBsplines(x, knots, kOrd, orderDeriv);
+std::cout << arr << std::endl;
+
+//Expected Output
+//  20
+//  40
+//-140
+//  80
+ * @endcode
+ * 
+ * @param x The x where to retrieve the spline values;
+ * @param knotsInput the knot points without ghost points
+ * @param kOrd order of the splines that are calculated
+ * @param nDeriv order of higest calculated derivative, default = 1
+ *               nDeriv = 0, just returns the B-splines without derivative.
+ * @return std::tuple<ArrayXd, int> that contains the splines in column 0 and then 
+ *         the n-th derivative in the nth column.
+ *         The other parameter is the highest index of the non-zero splines.
+ */
+std::tuple<ArrayXd, int> ndxBsplines(double x, ArrayXd &knotsInput, int kOrd, int nDeriv=1)
+{
+    ArrayXd output(kOrd);
+    
+    //the last value hold the knot of the point, so that we have a reference to our splines
+    ArrayXd splines = ArrayXd::Zero(kOrd);
+
+    ArrayXd knots(knotsInput.size()+2*kOrd-2);
+    //we add the ghost points for calculation of the splines in every point
+    knots << ArrayXd::Constant(kOrd-1, knotsInput(0)),knotsInput, ArrayXd::Constant(kOrd-1, knotsInput(last));
+
+    // we set the lowest layer BSplines manually
+    splines(0) = 1;
+
+    int i = kOrd-1; //we start at the first non ghost point
+
+    //if x == knot we have to reduce the index manually by one
+    if (x == knotsInput(last))
+    {
+        //special case is granted to the end ghost points as we want the first one here
+        //we want the index where the last element starts, so no ghost points but the last real element
+        i = knotsInput.size()-2+i;
+    }
+    else
+    {
+        //we search for the knot left of the x-value
+        while (x >= knots(i))
+        {
+            i++;
+        }
+        i--;
+    }
+
+    //we iterate through to the k-2-th order in the triangular shape the problem
+    //then we stop to calculate the derivative, after that we continue for 2 more iterations.
+    for (int k = 1; k < kOrd-nDeriv; k++)
+    {
+        
+        splines(k) = (knots(i+1)-x)/(knots(i+1)-knots(i-k+1))*splines(k-1);
+        for (int j = 1; j < k; j++)
+        {
+            splines(k-j) = (x-knots(i-k+j)) / (knots(i+j)-knots(i-k+j)) * splines(k-j)
+                        +(knots(i+j+1)-x) / (knots(i+j+1)-knots(i-k+j+1)) * splines(k-j-1);
+        }
+        splines(0) = (x-knots(i))/(knots(i+k)-knots(i))*splines(0);
+
+    }
+    output = ndxBsplinesHelper(splines, knots, i, nDeriv);
     return {output, i};
 
 }
